d292f1329024b332c7dc1d72780ba7fdebe3b9cd
Douglas Collioni <dcollioni@gmail.com>
2016-10-17 23:00:06 -0200

    add blood centers export and list

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,13 @@
 import Hapi from 'hapi';
+import bloodCenters from '../data/bloodcenters.json';
+import { MongoClient } from 'mongodb';
+
+const url = 'mongodb://admin:admin@ds021999.mlab.com:21999/heroku_vtrpfspm';
+let mongo = {};
 
 const server = new Hapi.Server();
 server.connection({
-  port: 4000,
+  port: process.env.PORT || 4000,
 });
 
 server.route({
@@ -13,10 +18,40 @@ server.route({
   },
 });
 
+server.route({
+  method: 'GET',
+  path: '/bloodcenters/export',
+  handler(request, reply) {
+    const collection = mongo.collection('bloodcenters');
+
+    collection.insertMany(bloodCenters, (err, result) => (
+      reply(`${result.ops.length} Bloodcenters exported`)
+    ));
+  },
+});
+
+server.route({
+  method: 'GET',
+  path: '/bloodcenters',
+  handler(request, reply) {
+    const collection = mongo.collection('bloodcenters');
+
+    collection.find({}).toArray((err, docs) => (
+      reply(docs)
+    ));
+  },
+});
+
 server.start(err => {
   if (err) {
     throw err;
   }
 
   console.log('Server running at:', server.info.uri);
+
+  MongoClient.connect(url, (err2, db) => {
+    console.log('Connected to mongo db');
+    mongo = db;
+    // db.close();
+  });
 });
